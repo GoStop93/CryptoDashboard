1a2c861488b0b9d7d2684fabfc1bb517dd9b3100
Mike <rj1993@yandex.ru>
2022-11-30 13:29:53 +0100

    first

--- src/services/CryptoService.js
+++ src/services/CryptoService.js
@@ -5,34 +5,37 @@ import crypto from 'crypto-browserify';
 
 
 
+
+
+
+
 const useCryptoService = () => {
     const {loading, request, error, clearError} = useHttp();
 
-    // const api_key = 'U0agEbblySd8kSdn0R';
-    // const apiSecret = 'U5iElEDTAJLqOki3qs9UQLj8VWqR4MCFBqMV';
+    const api_key = 'U0agEbblySd8kSdn0R';
+    const apiSecret = 'U5iElEDTAJLqOki3qs9UQLj8VWqR4MCFBqMV';
 
-    const api_key = 'xxemLYPxfAs0cE3GYM';
-    const apiSecret = '3VKhuGWj8sRxsGfoTbxGxW0D3hmIdZ6NjHal';
+    // const api_key = 'xxemLYPxfAs0cE3GYM';
+    // const apiSecret = '3VKhuGWj8sRxsGfoTbxGxW0D3hmIdZ6NjHal';
+
 
     
     let timestamp = Date.now().toString();
 
     const expires = new Date().getTime() + 10000;
-    let sign;
-
-    // const sign = crypto.createHmac('sha256', apiSecret).update('GET/realtime' + expires).digest('hex');
 
 
+    const sign = crypto.createHmac('sha256', apiSecret).update('GET/realtime' + expires).digest('hex');
 
-    const getSignature = () => {
-        return sign = crypto.createHmac('sha256', apiSecret).update('GET/realtime' + expires).digest('hex')
-    }
+    // const getSignature = () => {
+    //     return sign = crypto.createHmac('sha256', apiSecret).update('GET/realtime' + expires).digest('hex')
+    // }
 
     const getFirstInformation = async () => {
-        getSignature();
-        const res = await request(`https://api.bybit.com/v2/private/wallet/balance?api_key=${api_key}&coin=BTC&timestamp=${timestamp}&sign=${sign}`)
+        const res = await request(`https://api-testnet.bybit.com/v2/private/wallet/balance?api_key=${api_key}&coin=BTC&timestamp=${timestamp}&sign=${sign}`)
         console.log(res);
     }
+
 
     const getPrices = async () => {
         const res = await request('https://api.bybit.com/v2/public/tickers');
